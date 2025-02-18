2f92a7d7a287c7694723b5b7b5d479a4ea13627f
Sietse <sietsedrent@gmail.com>
2025-02-18 18:00:51 +0100

    fix Bugs
    -Zorgen dat poortnummer goed is (die van de API)
    -Zorgen dat beide bestanden tegelijk gerund worden

--- UC2-Contactpagina/Showcase-Contactpagina/Controllers/ContactController.cs
+++ UC2-Contactpagina/Showcase-Contactpagina/Controllers/ContactController.cs
@@ -56,12 +56,12 @@ namespace Showcase_Contactpagina.Controllers
             //Hint: vergeet niet om de mailfunctionaliteit werkend te maken in ShowcaseAPI > Controllers > MailController.cs,
             //      nadat je een account hebt aangemaakt op Mailtrap (of een alternatief).
 
-            HttpResponseMessage response = await _httpClient.PostAsync("https://localhost:7150/api/mail", content);             // Vervang deze regel met het POST-request
+            HttpResponseMessage response = await _httpClient.PostAsync("https://localhost:7278/api/mail", content);             // Vervang deze regel met het POST-request
 
 
             if (!response.IsSuccessStatusCode)
             {
-                ViewBag.Message = "Er is iets misgegaan";
+                ViewBag.Message = $"Er is iets misgegaan. Statuscode: {response.StatusCode}, Response inhoud: {response.Content}";
                 return View();
             }
 
